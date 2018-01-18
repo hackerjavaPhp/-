63ab4779201299b80293aa96dd2845cef8e2fafb
Vlad <hackerjavaVlad@gmail.com>
2018-01-18 21:14:45 +0300

    Add files via upload

---
+++ jquery to-do-list/js/main.js
@@ -0,0 +1,28 @@
+$(function() {
+		// add eleemt
+
+		$("#add").on("click", function() {
+		var val = $("input").val();
+		if(val !== '') {
+
+		var elem = $("<li></li>").text(val);
+
+
+		$(elem).append("<button class='rem'>X</button>");
+
+
+		$("#mylist").append(elem);
+
+
+		$("input").val("");
+
+		// remove element
+
+		$(".rem").on("click", function() {
+			$(this).parent().remove();
+		});
+
+		}
+	});
+
+});
